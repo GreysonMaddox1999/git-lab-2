86f206bb7212b5419602d8e266f1e9f385008aa5
Greyson <gm740317@ohio.edu>
2021-01-27 17:19:13 -0500

    Hello Git Program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Greyson Maddox
+ *        Date: 1/27/2021
+ * Description: File that prints out "Hello Git!!" when run
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
